f8870f9bca237b651eac8f516dc74b5b05405cc2
p0ntan <pontus.akerberg@hotmail.com>
2023-11-07 06:19:53 +0100

    Text update

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # Test med punkter och polygoner
 
-Det här är bara ett litet test att se hur man kan kolla om en punkt är inom en polygon. Testar två olika npm paket där man endast använder koordinaterna.
+Det här är bara ett litet test att se hur man kan kolla om en punkt är inom en polygon och returenrar true eller false. Testar två olika npm paket där man endast använder koordinaterna.
 
 Testa
 ```
 npm install
 node index.js
+```
+
+Ger utskrift
+```
+# geo-point-in-polygon
+Innanför true
+Knappt utanför false
+Utanför false
+
+# point-in-polygon
+Innanför true
+Knappt utanför false
+Utanför false
 ```
 
 ## geo-point-in-polygon
--- index.js
+++ index.js
@@ -9,15 +9,14 @@ const insideCord = inside.features[0].geometry.coordinates;
 const outsideCloseCord = outsideClose.features[0].geometry.coordinates;
 const outsideCord = outside.features[0].geometry.coordinates;
 
+// https://www.npmjs.com/package/geo-point-in-polygon
+// bara två dependencies
+console.log("Innanför", geoPointInPolygon(insideCord, polygonCord));
+console.log("Knappt utanför", geoPointInPolygon(outsideCloseCord, polygonCord));
+console.log("Utanför", geoPointInPolygon(outsideCord, polygonCord));
 
 // https://www.npmjs.com/package/point-in-polygon
 // sjukt många dependencies 213 stk
 console.log("Innanför", pointInPolygon(insideCord, polygonCord));
 console.log("Knappt utanför", pointInPolygon(outsideCloseCord, polygonCord));
 console.log("Utanför", pointInPolygon(outsideCord, polygonCord));
-
-// https://www.npmjs.com/package/geo-point-in-polygon
-// bara två dependencies
-console.log("Innanför", geoPointInPolygon(insideCord, polygonCord));
-console.log("Knappt utanför", geoPointInPolygon(outsideCloseCord, polygonCord));
-console.log("Utanför", geoPointInPolygon(outsideCord, polygonCord));
